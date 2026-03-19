66cd12f8ebd82997f769a47de4f79c8ae852a784
Calebe Alves de Sousa <calebinhuusousa@gmail.com>
2026-03-19 12:06:44 -0300

    versao rotas e vetores

--- appEJS/app.js
+++ appEJS/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var ferrariRouter = require('./routes/ferrari');
+var esporteRouter = require('./routes/esporte');
+var bolasRouter = require('./routes/bolas');
 
 var app = express();
 
@@ -22,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/ferrari', ferrariRouter);
-
+app.use('/esporte', esporteRouter);
+app.use('/bolas', bolasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
